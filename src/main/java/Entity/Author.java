81260b0f2705d006a39eac74fa56ed6813907050
violagremi <viola.gremi@gmail.com>
2025-09-13 14:49:37 +0200

    Class Authorr

--- src/main/java/Entity/Author.java
+++ src/main/java/Entity/Author.java
@@ -1,4 +1,28 @@
 package Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nationality")
+    private String nationaity;
+
+    @Column(name = "birthDate")
+    private LocalDate birthDate;
 }
