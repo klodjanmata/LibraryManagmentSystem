f18ce16df03d2cc48e5eb717f5281c73c4a2394a
Sara <sarabrinja01@gmail.com>
2025-09-14 11:44:41 +0200

    my bro

---
+++ src/main/java/MenuManager.java
@@ -0,0 +1,28 @@
+public class MenuManager {
+
+
+public static void MenuManager (){
+    System.out.println("\n");
+    System.out.println(" ********** Welcome to Library Management System  **********");
+    System.out.println("Chose your action!");
+    System.out.println("1. Add Author");
+    System.out.println("2. Print All Authors");
+    System.out.println("3. Add Book");
+    System.out.println("4. Print All Books");
+    System.out.println("5. Add Genre");
+    System.out.println("6. Print All Genres");
+    System.out.println("7. Add Member");
+    System.out.println("8. Print all Members");
+    System.out.println("9. Add Borrow Record");
+    System.out.println("10. Print all BorrowRecords");
+    System.out.println("0. Shut down");
+}
+
+public static void filterMenu(){
+    System.out.println("Chose your filter!");
+    System.out.println("1. Filter by Author");
+    System.out.println("2. Filter by Book");
+    System.out.println("3. Filter by Genre");
+
+}
+}
