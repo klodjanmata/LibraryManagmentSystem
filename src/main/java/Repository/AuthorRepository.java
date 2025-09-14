d185933d0e784383847003c64e8419682013d6c9
violagremi <viola.gremi@gmail.com>
2025-09-14 10:22:39 +0200

    repositories

---
+++ src/main/java/Repository/AuthorRepository.java
@@ -0,0 +1,74 @@
+package Repository;
+
+import Entity.Author;
+import Util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class AuthorRepository {
+
+    public Author create(Author author){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            session.persist(author);
+            transaction.commit();
+            return author;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return author;
+    }
+
+    public Author read(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Author.class,id);
+        }
+    }
+
+    public Author update(Author author){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            session.merge(author);
+            transaction.commit();
+            return author;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return author;
+    }
+
+    public Author delete(Author author){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            session.remove(author);
+            transaction.commit();
+            return author;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return author;
+    }
+
+    public List<Author> findAll() {
+        Transaction transaction = null;
+        List<Author> authors = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            authors = session.createQuery("from Author", Author.class).list();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        return authors;
+    }
+
+}
